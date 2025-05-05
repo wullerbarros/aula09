1d6c860917f57a31fab1f949f25064046f07408b
wullerbarros <wuller1987@gmail.com>
2025-05-05 16:53:36 -0300

    metadeleitura

---
+++ index.js
@@ -0,0 +1,36 @@
+const searchInput = document.getElementById("searchInput")
+
+const produtos = [
+    {
+        img: './imagem/pokemon.jpg',
+        titulo:'Pokémon Scarlet e Violet',
+        categoria:'Games',
+        preco:420.00,
+        desconto:true
+
+    },
+    {
+        img: './imagem/pokemon.jpg',
+        titulo:'Pokémon Scarlet e Violet',
+        categoria:'Games',
+        preco:420.00,
+        desconto:true
+
+    },
+    {
+        img: './imagem/pokemon.jpg',
+        titulo:'Pokémon Scarlet e Violet',
+        categoria:'Games',
+        preco:420.00,
+        desconto:false
+
+    },
+    {
+        img: './imagem/pokemon.jpg',
+        titulo:'Pokémon Scarlet e Violet',
+        categoria:'Games',
+        preco:420.00,
+        desconto:false
+
+    }
+]
